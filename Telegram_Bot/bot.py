5560a0c720cf12ea8a997270e3f53c89bfaee14f
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2023-05-05 06:47:37 +0530

    show tel_id to user

--- Telegram_Bot/bot.py
+++ Telegram_Bot/bot.py
@@ -24,7 +24,7 @@ def start(message):
     appointment = types.InlineKeyboardButton("Appointment 🩺", url='http://127.0.0.1:5000/appointment')
     keyboard.add(emergency, hospitals, appointment)
 
-    bot.send_message(message.chat.id, "Welcome to Life Bot!😊 \n\n I am here to help you with your emergency needs and services.⚕️🩸 \n\n What may I assist you with?", reply_markup=keyboard)
+    bot.send_message(message.chat.id, f"Welcome to Life Bot!😊 \n\n I am here to help you with your emergency needs and services.⚕️🩸 \n\n What may I assist you with?\n\nYour Telegram ID: {message.chat.id}", reply_markup=keyboard)
 
 
 def handle_hospitals(message):
